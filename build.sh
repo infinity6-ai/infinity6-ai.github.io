f5a7ce36f7142d893b9954a34997c5f333765bfa
murer <paulo.murer@infinity6.ai>
2024-10-21 18:53:33 -0300

    x

--- build.sh
+++ build.sh
@@ -89,7 +89,7 @@ function cmd_download_videos() {
 function cmd_fix_localhost() {
   local _k=""
   grep -nro 'http:[/\\]\+localhost:8080' docs | cut -d':' -f1 | sort | uniq | while read _k; do
-    sed -i 's/http:[\\\/]\+localhost:8080/https:\/\/www-staging.infinity6.ai/g' "$_k"
+    sed -i 's/http:[\\\/]\+localhost:8080/https:\/\/www.infinity6.ai/g' "$_k"
   done
 }
 
--- docs/xmlrpc.php
+++ docs/xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://www-staging.infinity6.ai</homePageLink>
+		<homePageLink>https://www.infinity6.ai</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www-staging.infinity6.ai/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www-staging.infinity6.ai/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www-staging.infinity6.ai/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www-staging.infinity6.ai/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www-staging.infinity6.ai/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www.infinity6.ai/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.infinity6.ai/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.infinity6.ai/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www.infinity6.ai/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.infinity6.ai/wp-json/" />
 			</apis>
 	</service>
 </rsd>
