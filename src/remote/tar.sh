3c7fe378fd0f2e59c8d747e965e02d3290717f18
murer <paulo.murer@infinity6.ai>
2024-10-21 18:02:44 -0300

    x

--- src/remote/tar.sh
+++ src/remote/tar.sh
@@ -1,3 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
-cat /tmp/list.filtered.txt | xargs tar czf /tmp/export.tar.gz
+# cat /tmp/list.filtered.txt | xargs tar czf /tmp/export.tar.gz
+# tar czf /tmp/export.tar.gz html
+
+rm -rf /tmp/html/ || true
+rsync -a --files-from=/tmp/list.filtered.txt ./ /tmp/html/
+cd /tmp/html
+tar czf export.tar.gz html
